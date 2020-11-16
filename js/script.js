9a9f079871a9631ceb6fd794a7da791baee5dc50
Iacopo <iacopo.ghila@gmail.com>
2020-11-16 13:56:21 +0100

    Commenti

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@
 // Stampa la lista ordinata alfabeticamente.
 // Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
 
+//chiedo all'utente il cognome
 var cognomeUtente = prompt("Inserisci il tuo cognome");
+//Trasformo la prrima lettera in maiuscola
 cognomeUtente = cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1);
+
 var cognomi = ['Pesce', 'Grimaldi', 'Passaro', 'Castenetto', 'Rossi', 'Verdi', 'Bianchi'];
+//aggiungo il cognome all'array
 cognomi.push(cognomeUtente);
 
-
 console.log(cognomi);
+//stampo la lista ordinata
 console.log(cognomi.sort());
-// for (var i = 0; i < cognomi.length; i++) {
-//
-// }
